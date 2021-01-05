c0cb1a6d5a96f7d0f3af18b627923896d5847b46
md-afroz-vst-au4 <afrozjmi102@gmail.com>
2021-01-05 13:53:35 +0530

    new local_setting

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -125,3 +125,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# If working locally allow debug mode and localhost origin else debug= False
+try:
+    from .local_settings import *
+
+except ImportError:
+    print("Looks like no local file. You must be on production")
