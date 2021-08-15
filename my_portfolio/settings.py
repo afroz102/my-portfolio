14ed1a95ecc8001bc8b610271ab503f5e4f96fc6
md-afroz-vst-au4 <afrozjmi102@gmail.com>
2021-08-15 16:18:57 +0530

    some bug fixed

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'my_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
